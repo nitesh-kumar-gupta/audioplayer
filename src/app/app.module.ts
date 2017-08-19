ce70b5f16e5b509ccddd808d848c940e0d3356c1
Nitesh Kumar Gupta <nitesh.gupta@venturepact.com>
2017-08-19 21:45:28 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicAudioModule } from 'ionic-audio';
 import { MyApp } from './app.component';
+
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
@@ -14,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicAudioModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component} from '@angular/core';
+import { AudioProvider } from 'ionic-audio';
 import { NavController } from 'ionic-angular';
+import {ITrackConstraint} from "ionic-audio/dist/ionic-audio-interfaces";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements AfterContentInit{
+  myTracks: any;
+  selectedTrack: number;
+  isPlaying: boolean[];
+  constructor(public navCtrl: NavController, private _audioProvider: AudioProvider) {
+    this.myTracks = [{
+      src: 'https://archive.org/download/JM2013-10-05.flac16/V0/jm2013-10-05-t12-MP3-V0.mp3',
+      artist: 'John Mayer',
+      title: 'Why Georgia',
+      art: 'https://pbs.twimg.com/profile_images/822251283406934016/pQC2vwZT.jpg',
+      preload: 'metadata'
+    },{
+      src: 'https://archive.org/download/JM2013-10-05.flac16/V0/jm2013-10-05-t30-MP3-V0.mp3',
+      artist: 'John Mayer',
+      title: 'Who Says',
+      art: 'https://pbs.twimg.com/profile_images/822251283406934016/pQC2vwZT.jpg',
+      preload: 'metadata'
+    }];
+    this.selectedTrack = 0;
+  }
 
-  constructor(public navCtrl: NavController) {
+  ngAfterContentInit() {
+    this.isPlaying = [];
+    this.myTracks.forEach((track) => {
+      this._audioProvider.create(track);
+      this.isPlaying.push(false);
+    });
+  }
 
+  playSelectedTrack(num) {
+    if(num !== this.selectedTrack) {
+      this._audioProvider.stop(this.selectedTrack);
+      this.isPlaying[this.selectedTrack] = false;
+    }
+    this.selectedTrack = num;
+    this._audioProvider.play(this.selectedTrack);
+    this.isPlaying[num] = true;
+  }
+
+  pauseSelectedTrack(num) {
+    this.selectedTrack = num;
+    this._audioProvider.pause(this.selectedTrack);
+    this.isPlaying[num] = false;
+  }
+
+  onTrackFinished(track: any) {
+    console.log('Track finished', track)
   }
 
 }
